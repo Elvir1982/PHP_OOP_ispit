bbc92bd2b77fcd5e1a70f31f1c2ec0181a61f477
Elvir1982 <110693607+Elvir1982@users.noreply.github.com>
2023-03-27 21:24:28 +0200

    Update Bootstrap.php

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -1,11 +1,8 @@
 <?php
 namespace Core;
 class Bootstrap{
-    public function boot(){
-       
+    public function boot(){     
         define ('CONFIG',ROOT.'/config');
         require CONFIG.'/database.php';
-
-
     }
 }
